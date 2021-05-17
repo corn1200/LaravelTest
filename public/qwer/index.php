ae83a17f2a60347b5dc88af4f0bf343ae40304bc
김현승 <khs@e-no.tv>
2021-05-17 14:49:04 +0900

    first commit

---
+++ public/qwer/index.php
@@ -0,0 +1,3 @@
+khs2.innoi.kr<br>
+Create : 2021-05-17 13:56:35<br>
+<?phpinfo();
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $greeting = 'Hello';
+    $name = 'um';
+
+    // 뷰에 데이터를 바인딩하는 여러 방법
+
+    // 1. 메소드
+    // return view('index')->with([
+    //     'greeting' => $greeting,
+    //     'name'     => $name
+    // ]);
+
+    // 2. 인자로 넘기기
+    // return view('index', [
+    //     'greeting' => $greeting,
+    //     'name'     => $name
+    // ]);
+
+    // 3. 인자로 넘기기(compact 메소드)
+    // return view('index', 
+    //     compact('greeting', 'name')
+    // );
+
+    // 4. view Property를 이용하기
+    $view = View('index');
+    $view->greeting = "Hey~ WaSuuuuup";
+    $view->name = 'everyone';
+
+    return $view;
+});
